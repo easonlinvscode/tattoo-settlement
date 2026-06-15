f2c7b3f43982d97a9473cb21ccbd06f759685abf
Eason <crazyboy199371@gmail.com>
2026-06-15 23:21:21 +0800

    新增 GitHub Pages 自動部署設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/tattoo-settlement/',
   plugins: [vue()],
   resolve: {
     alias: {
